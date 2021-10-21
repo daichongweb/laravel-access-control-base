128292b6bce6451d057772547b69767f17369036
daichongweb <daichongweb@foxmail.com>
2021-10-21 17:01:48 +0800

    feature: #1021 授权

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
         $data = [];
         if ($code = $request->get('code')) {
             $service = new AuthService();
-            $service->token = EnterpriseRedis::get();
+            $service->token = 'e6l8vHh8nG6L-DmAjINWuR5CIGApqMsQD5u1qQjMwzyB1d-clSNsgkMoxHXceLJ_fPVWk9b4-G4uaGe_7rsSQ_Lus25Kj2jGfqRBk4JINVFkKcyNQPWkPKXXZYua0HZA9YmyaDPYA1QxAVnyNnGKAHKn3xv0iWjzjCWZ6vuvl7buayq5xFBT1pfG8WQppY07EjucqCtIirPGBNF02ap8sg';
             $data = $service->user($code);
             var_dump($data);
         }
