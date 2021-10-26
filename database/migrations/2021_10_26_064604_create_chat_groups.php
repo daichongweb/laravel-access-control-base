6123948932a6d591e6dc269f9e7c0dd0243159fa
daichongweb <daichongweb@foxmail.com>
2021-10-26 16:36:52 +0800

    feature: #1026 修改注释

--- database/migrations/2021_10_26_064604_create_chat_groups.php
+++ database/migrations/2021_10_26_064604_create_chat_groups.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * 客户群表
+ */
 class CreateChatGroups extends Migration
 {
     /**
--- database/migrations/2021_10_26_074257_create_members.php
+++ database/migrations/2021_10_26_074257_create_members.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * 企业成员表
+ */
 class CreateMembers extends Migration
 {
     /**
