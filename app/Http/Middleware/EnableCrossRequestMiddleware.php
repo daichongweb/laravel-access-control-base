4020c0f24898d3983db0ea98b286601992c757c1
daichongweb <daichongweb@foxmail.com>
2021-11-06 17:29:57 +0800

    feature: #1106 跨域处理

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -23,7 +23,7 @@ class EnableCrossRequestMiddleware
             return $response;
         }
         $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN,key');
         $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS, DELETE');
         $response->header('Access-Control-Allow-Credentials', 'true');
