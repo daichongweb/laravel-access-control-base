b30e26d5e73a777c4de49487e87c07c8fdd5c805
daichongweb <daichongweb@foxmail.com>
2021-11-08 14:27:19 +0800

    feature: #1108 图片限制删除

--- app/Http/Requests/UploadRequest.php
+++ app/Http/Requests/UploadRequest.php
@@ -16,7 +16,7 @@ class UploadRequest extends BaseRequest
     public function rules(): array
     {
         return [
-            'image' => 'required|mimes:jpeg,png,jpeg,gif|max:2048|dimensions:min_width=200,min_height=200,max_width=1000,max_height=1000'
+            'image' => 'required|mimes:jpeg,png,jpeg,gif|max:2048'
         ];
     }
 
@@ -26,7 +26,7 @@ class UploadRequest extends BaseRequest
             'image.required' => '请上传图片',
             'image.mimes' => '图片必须是 jpeg, jpg, png, gif 格式的图片',
             'image.max' => '图片过大',
-            'image.dimensions' => '图片的尺寸错误，宽和高需要200px以上，1000px以下'
+//            'image.dimensions' => '图片的尺寸错误，宽和高需要200px以上，1000px以下'
         ];
     }
 }
